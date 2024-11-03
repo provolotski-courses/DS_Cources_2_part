3dbd1ac9ce43f44555676372f8932ee450e06045
provolotski <provolotski@gmail.com>
2024-11-03 22:34:21 +0300

    отредактировал первую и добавил вторую домашку

--- HW1/homework1.py
+++ HW1/homework1.py
@@ -1,9 +1,16 @@
-import numpy as np
+import seaborn as sns
 import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 
 def eda_report(analyzed_dataset):
-    """Проведение EDA-анализа над переданным датасетом"""
+    """
+    Проведение EDA-анализа над переданным датасетом
+    """
     """Вывод статистики по дата фрейму"""
     print('***---- head  ------***')
     print(analyzed_dataset.head())
@@ -20,44 +27,164 @@ def eda_report(analyzed_dataset):
         print(f'гранулярность данных {analyzed_dataset[col].nunique()}')
         print('Частота появления каждого значения:')
         print(analyzed_dataset[col].value_counts())
-    # выводим значения подмен
+
+
+"""Вычисление метрик"""
+
+
+def show_metrics(model_name, y_test, y_pred):
+    """
+    Отображение метрик для модели
+    """
+    mae = mean_absolute_error(y_test, y_pred)
+    mse = mean_squared_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
+
+    print(f'\n*****************')
+    print(f'Метрики качества   метода {model_name}:')
+    print(f'*****************')
+    print(f'MAE: {mae:.4f}')
+    print(f'MSE: {mse:.4f}')
+    print(f'R²: {r2:.4f}')
+
+
+def preprocessing_input_data(dataset):
+    """
+    Предобработка датасета:
+        просмотр статистики и определение актуальных параметров
+    """
+    eda_report(dataset)
+
+    # последовательное преобрабование даты переписи из строки в дату а затем в число
+    dataset['Reference Date'] = dataset['Reference Date'].str.replace("0 December", "1 December")
+    dataset['Reference Date'] = pd.to_datetime(dataset['Reference Date'])
+    dataset['Reference Date'] = dataset['Reference Date'].astype('int64')
+
+    # отбор данных без разбивки по полу
+    dataset = dataset[dataset['Sex Code'] == 0]
+    # определение актуальных столбцов
+    data = dataset[["Country or Area Code", "Reference Date", "Code of City", "Code of City type"]]
+    # определение целевой функции
+    target = dataset["Value"]
+    # возвращаем значения
+    return data, target
 
 
 
 
-def preprocessing_input_data (pd):
-    eda_report(pd)
-    pd = pd.drop('Country or Area Code', axis=1)
-    pd = pd.drop('Reference Date', axis=1)
-    pd = pd.drop('Area Code', axis=1)
-    pd = pd.drop('Area', axis=1)
-    pd = pd[pd['Sex Code'] ==0]
-    pd = pd.drop('Sex Code', axis=1)
-    pd = pd.drop('Sex', axis=1)
-    pd = pd.drop('Code of City', axis=1)
-    pd = pd.drop('Code of City type', axis=1)
-    pd = pd.drop('Record Type Code', axis=1)
-    pd = pd.drop('Record Type', axis=1)
-    pd = pd.drop('Reliability Code', axis=1)
-    pd = pd.drop('Reliability', axis=1)
-    pd = pd.drop('Source Year', axis=1)
-    pd = pd.drop('Value Footnotes', axis=1)
-    pd.to_csv('data/newdataset.csv')
 
+def show_diagrams(model_name, dataset, y_test, y_pred, model):
+    """
+    Визуализация результатов на графиках
+    """
+    #  Для удобства визуализации доббавим автоинкрементный столбец
+    dataset.insert(0, 'ID', range(0, len(dataset)))
+    # Формируем фрейм для отображения результатов
+    results = pd.DataFrame({
+        'ID': dataset.loc[y_test.index, 'ID'].values,
+        'Actual': y_test.values,
+        'Predicted': y_pred
+    }).sort_values('ID').reset_index(drop=True)
 
-    return pd
+    # Графиков много не бывает
+    # Scatter plot с линией y=x
+    plt.figure(figsize=(8, 8))
+    sns.scatterplot(x='Actual', y='Predicted', data=results, alpha=0.6)
+    plt.plot([results['Actual'].min(), results['Actual'].max()],
+             [results['Actual'].min(), results['Actual'].max()],
+             color='red', linestyle='--', label='y = x')
+    plt.xlabel('Фактические значения "Численность населения"')
+    plt.ylabel('Предсказанные значения "Численность населения"')
+    plt.title(f'Фактические vs Предсказанные значения для модели {model_name}')
+    plt.legend()
+    plt.show()
 
-def linear_regression_model(pd):
-    print('================================new one=========================================')
-    eda_report(pd)
+    # Линейный график для последовательности наблюдений
+    plt.figure(figsize=(14, 6))
+    plt.plot(results['ID'], results['Actual'], label='Фактические значения', color='blue')
+    plt.plot(results['ID'], results['Predicted'], label='Предсказанные значения', color='red', alpha=0.7)
+    plt.xlabel('ID (Порядковый номер наблюдения)')
+    plt.ylabel('Global_active_power')
+    plt.title(f'Фактические и Предсказанные значения для модели {model_name}')
+    plt.legend()
+    plt.show()
+
+    # Распределение ошибок предсказания
+    results['Error'] = results['Actual'] - results['Predicted']
+    plt.figure(figsize=(10, 6))
+    sns.histplot(results['Error'], bins=50, kde=True, color='purple')
+    plt.xlabel('Ошибка (Фактические - Предсказанные)')
+    plt.title(f'Распределение ошибок предсказания для модели {model_name}')
+    plt.show()
+
+    # Боксплот ошибок
+    plt.figure(figsize=(8, 6))
+    sns.boxplot(x=results['Error'], color='lightgreen')
+    plt.xlabel('Ошибка (Фактические - Предсказанные)')
+    plt.title(f'Боксплот ошибок предсказания для модели {model_name}')
+    plt.show()
+
+    # Корреляционная матрица между фактическими и предсказанными значениями
+    plt.figure(figsize=(6, 4))
+    corr = results[['Actual', 'Predicted']].corr()
+    sns.heatmap(corr, annot=True, cmap='coolwarm', vmin=0, vmax=1)
+    plt.title(f'Корреляция между фактическими и предсказанными значениями для модели {model_name}')
+    plt.show()
+
+    #  Важность признаков
+    if model:
+        feature_names = ["Country or Area Code", "Reference Date", "Code of City", "Code of City type"]
+        feature_importances = pd.Series(model.feature_importances_, index=feature_names)
+        plt.figure(figsize=(10, 6))
+        sns.barplot(x=feature_importances.values, y=feature_importances.index, color='skyblue')
+        plt.xlabel('Важность признака')
+        plt.ylabel('Признак')
+        plt.title(f'Важности признаков в модели {model_name}')
+        plt.show()
+
+        # График 6: Анализ остатков
+    plt.figure(figsize=(10, 6))
+    sns.scatterplot(x=y_pred, y=results['Error'], alpha=0.6)
+    plt.axhline(0, color='red', linestyle='--')
+    plt.xlabel('Предсказанные значения')
+    plt.ylabel('Остатки (Фактические - Предсказанные)')
+    plt.title(f'Анализ остатков для модели {model_name}')
+    plt.show()
+
 
 def main():
+    # Читаем файл с данными
     file_name = 'data/UNdata_Export_20241025_142506414.txt'
     dataset = pd.read_csv(file_name, sep=';')
-    dataset = preprocessing_input_data(dataset)
-    city = 'MINSK'
-    linear_regression_model(dataset[dataset['City'] == city].drop('City', axis=1).drop('Country or Area', axis=1).drop('City type', axis=1))
+    # проверяем и чистим данные
+    data, target = preprocessing_input_data(dataset)
+    # Разбиваем на тренировочную и тестовую выборки
+    X_train, X_test, Y_train, Y_test = train_test_split(data, target, test_size=0.2, random_state=13)
 
+    # Создание и обучение модели Линейной регрессии
+
+    model1 = LinearRegression()
+    model1_name = 'Линейная регрессия'
+    model1.fit(X_train, Y_train)
+
+    # Предсказание на тестовой выборке
+    y_pred_linear = model1.predict(X_test)
+    # показываю метрики
+    show_metrics(model1_name, Y_test, y_pred_linear)
+
+    # поскольку вряд ли  население городов это линейная функция то паралельно создаю и обучаю графиентный бустинг
+
+    model2 = GradientBoostingRegressor()
+    model2_name = 'Градиентный бустинг'
+    model2.fit(X_train, Y_train)
+
+    # Предсказание на тестовой выборке
+    y_pred_boost = model2.predict(X_test)
+    # показываю метрики
+    show_metrics(model2_name, Y_test, y_pred_boost)
+    # отображаю визуализацию для обеих моделей
+    show_diagrams(model1_name, dataset, Y_test, y_pred_linear, None)
+    show_diagrams(model2_name, dataset, Y_test, y_pred_boost, model2)
 
 
 if __name__ == '__main__':
---
+++ HW2/recurrentencoder.py
@@ -0,0 +1,87 @@
+import numpy as np
+from keras.src.utils import pad_sequences
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, LSTM, Dense, Embedding
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import Attention
+
+# загружаем данные из файла по-строчно
+with open('data/input_text.txt', 'r', encoding='utf-8') as f:
+    texts = f.readlines()
+print(f'text is {texts}')
+# Параметры
+sequence_length = 3  # Количество слов в последовательности
+embedding_dim = 50   # Размерность эмбеддингов
+latent_dim = 64      # Размерность скрытого состояния
+
+# Подготовка данных
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(texts) #формируем токены
+total_words = len(tokenizer.word_index) + 1  #+1 поскольку счет с нуля
+sequences = []
+# формируем последовательность токенов
+for line in texts:
+    token_list = tokenizer.texts_to_sequences([line])[0]
+    for i in range(1, len(token_list)):
+        n_gram_sequence = token_list[max(0, i - sequence_length):i + 1]
+        sequences.append(n_gram_sequence)
+
+# Дополнение последовательностей нулями для одинаковой длины
+sequences = pad_sequences(sequences, maxlen=sequence_length + 1, padding='pre')
+X, y = sequences[:, :-1], sequences[:, -1]
+
+# One-hot представление целевых слов
+y = to_categorical(y, num_classes=total_words)
+
+
+# Энкодер
+encoder_inputs = Input(shape=(sequence_length,))
+x = Embedding(total_words, embedding_dim, input_length=sequence_length)(encoder_inputs)
+encoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True)
+encoder_outputs, state_h, state_c = encoder_lstm(x)
+
+
+# Механизм внимания
+attention = Attention()
+context_vector = attention([encoder_outputs, encoder_outputs])
+
+# Декодер
+decoder_lstm = LSTM(latent_dim)
+decoder_outputs = decoder_lstm(context_vector, initial_state=[state_h, state_c])
+decoder_dense = Dense(total_words, activation='softmax')
+output = decoder_dense(decoder_outputs)
+
+
+# Создание модели
+model = Model(encoder_inputs, output)
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Обучение модели
+model.fit(X, y, epochs=1000, batch_size=8, verbose=1) # отрабатываем на 1000 эпох
+
+# Функция для генерации следующего слова
+
+def predict_next_word(model, tokenizer, text, sequence_length):
+    token_list = tokenizer.texts_to_sequences([text])[0]
+    token_list = token_list[-sequence_length:]
+    token_list = pad_sequences([token_list], maxlen=sequence_length, padding='pre')
+    predicted = model.predict(token_list, verbose=0)
+    predicted_word_index = np.argmax(predicted)
+    for word, index in tokenizer.word_index.items():
+        if index == predicted_word_index:
+            return word
+    return ""
+
+
+#Рекуррентный автоэнкодер с механизмом внимания для предсказания последующих слов в тексте
+# Пример предсказания
+input_text = "рекурентный автоэнкодер "
+# чтобы не было слишком скучно генерируем последовательно 10 слов
+# для данной входной фразы результат: "рекурентный автоэнкодер  состоит из двух основных частей кодировщика и декодировщика результаты случаях"
+for i in range(0,10):
+    predicted_word = predict_next_word(model, tokenizer, input_text, sequence_length)
+    input_text = input_text+' '+ predicted_word
+
+print(f"Итоговая фраза: {input_text}")
